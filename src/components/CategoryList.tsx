476030b5b85696de5e9c13bcf5004531c4052750
Elddar00 <eldaridrizovic00@gmail.com>
2024-08-02 18:55:50 +0200

    E-commerce App Fetch Categories

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,146 +1,34 @@
+import { wixClientServer } from "@/lib/wixClientServer";
 import Image from "next/image";
 import Link from "next/link";
 
-const CategoryList = () => {
+const CategoryList = async () => {
+  const wixClient = await wixClientServer();
+
+  const cats = await wixClient.collections.queryCollections().find();
   return (
     <div className="px-4 overflow-x-scroll scrollbar-hide">
       <div className="flex gap-4 md:gap-8">
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/14466162/pexels-photo-14466162.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-xl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/18075558/pexels-photo-18075558/free-photo-of-hoops-earrings-in-ear.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-xl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/15064039/pexels-photo-15064039/free-photo-of-jewelry-blocks-in-a-gold-chain.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-xl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/14111399/pexels-photo-14111399.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-xl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/13160940/pexels-photo-13160940.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-xl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/8656237/pexels-photo-8656237.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-xl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/6716444/pexels-photo-6716444.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-xl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
-        <Link
-          href="/list?cat=test"
-          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
-        >
-          <div className="relative bg-slate-100 w-full h-96">
-            <Image
-              src="https://images.pexels.com/photos/5370643/pexels-photo-5370643.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-              alt=""
-              fill
-              sizes="20vw"
-              className="object-cover"
-            />
-          </div>
-          <h1 className="mt-8 font-light text-xl tracking-wide">
-            Category Name
-          </h1>
-        </Link>
+        {cats.items.map((item) => (
+          <Link
+            href={`/list?cat=${item.slug}`}
+            className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+            key={item._id}
+          >
+            <div className="relative bg-slate-100 w-full h-96">
+              <Image
+                src={item.media?.mainMedia?.image?.url || "cat.png"}
+                alt=""
+                fill
+                sizes="20vw"
+                className="object-cover"
+              />
+            </div>
+            <h1 className="mt-8 font-light text-xl tracking-wide">
+              {item.name}
+            </h1>
+          </Link>
+        ))}
       </div>
     </div>
   );
