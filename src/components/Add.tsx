cb5e202e34832051f3fef92b8ea681fd521f4b6d
Elddar00 <eldaridrizovic00@gmail.com>
2024-08-10 17:36:55 +0200

    Add basic cart functionalities (add)

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useWixClient } from "@/hooks/useWixClient";
 import { useState } from "react";
 
 const Add = ({
@@ -24,6 +25,23 @@ const Add = ({
     if (type === "i" && quantity < stockNumber) {
       setQuantity((prev) => prev + 1);
     }
+  };
+
+  const wixClient = useWixClient();
+
+  const addItem = async () => {
+    const response = await wixClient.currentCart.addToCurrentCart({
+      lineItems: [
+        {
+          catalogReference: {
+            appId: process.env.NEXT_PUBLIC_WIX_APP_ID!,
+            catalogItemId: productId,
+            ...(variantId && { options: { variantId } }),
+          },
+          quantity: quantity,
+        },
+      ],
+    });
   };
   return (
     <div className="flex flex-col gap-4">
@@ -55,7 +73,10 @@ const Add = ({
             </div>
           )}
         </div>
-        <button className="w-36 text-sm rounded-3xl ring-1 ring-boja text-boja py-2 px-4 hover:bg-boja hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none">
+        <button
+          onClick={addItem}
+          className="w-36 text-sm rounded-3xl ring-1 ring-boja text-boja py-2 px-4 hover:bg-boja hover:text-white disabled:cursor-not-allowed disabled:bg-pink-200 disabled:text-white disabled:ring-none"
+        >
           Add to Cart
         </button>
       </div>
