a4d7ea9812b8e5899e86e9498d9cc6fa17aa8190
Elddar00 <eldaridrizovic00@gmail.com>
2024-08-15 20:43:30 +0200

    Profile Page - Add Profile Form Design

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import UpdateButton from "@/components/UpdateButton";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { members } from "@wix/members";
 import Link from "next/link";
@@ -26,7 +27,51 @@ const ProfilePage = async () => {
 
   return (
     <div className="flex flex-col md:flex-row gap-24 md:h-[calc(100vh-180px)] items-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-      <div className="w-full md:w-1/2">User</div>
+      <div className="w-full md:w-1/2">
+        <h1 className="text-2xl">Profile</h1>
+        <form action="" className="mt-12 flex flex-col gap-4">
+          <label className="text-sm text-gray-700">Username</label>
+          <input
+            type="text"
+            name="username"
+            placeholder={user.member?.profile?.nickname || "john"}
+            className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+          />
+          <label className="text-sm text-gray-700">First Name</label>
+          <input
+            type="text"
+            name="firstName"
+            placeholder={user.member?.contact?.firstName || "John"}
+            className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+          />
+          <label className="text-sm text-gray-700">Surname</label>
+          <input
+            type="text"
+            name="lastName"
+            placeholder={user.member?.contact?.lastName || "Prezime"}
+            className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+          />
+          <label className="text-sm text-gray-700">Phone</label>
+          <input
+            type="text"
+            name="phone"
+            placeholder={
+              (user.member?.contact?.phones &&
+                user.member?.contact?.phones[0]) ||
+              "+381 64 22 22 33 9"
+            }
+            className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+          />
+          <label className="text-sm text-gray-700">E-mail</label>
+          <input
+            type="email"
+            name="email"
+            placeholder={user.member?.loginEmail || "ime@gmail.com"}
+            className="ring-1 ring-gray-300 rounded-md p-2 max-w-96"
+          />
+          <UpdateButton />
+        </form>
+      </div>
       <div className="w-full md:w-1/2">
         <h1 className="text-2xl">Orders</h1>
         <div className="mt-12 flex flex-col">
