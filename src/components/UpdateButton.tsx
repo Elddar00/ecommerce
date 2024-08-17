3adbddde699c2ab6095f2e1f6d7b23fe525fa592
Elddar00 <eldaridrizovic00@gmail.com>
2024-08-17 22:45:51 +0200

    UpdateButton - PT

--- src/components/UpdateButton.tsx
+++ src/components/UpdateButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 const UpdateButton = () => {
+  // change div to button
   return (
     <button className="bg-boja text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed max-w-96">
       Update
