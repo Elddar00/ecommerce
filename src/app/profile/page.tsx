234452a78be6ada9bdc72cac7b9ca4e593d1a5ae
Elddar00 <eldaridrizovic00@gmail.com>
2024-08-23 02:57:32 +0200

    Clear code and fix small Bug

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -14,7 +14,7 @@ const ProfilePage = async () => {
   console.log(user);
 
   if (!user.member?.contactId) {
-    <div className="">Not logged in!</div>;
+    return <div className="text-center mt-8">Not logged in!</div>;
   }
 
   const orderRes = await wixClient.orders.searchOrders({
@@ -29,7 +29,7 @@ const ProfilePage = async () => {
     <div className="flex flex-col md:flex-row gap-24 md:h-[calc(100vh-180px)] items-center px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
       <div className="w-full md:w-1/2">
         <h1 className="text-2xl">Profile</h1>
-        <form action="" className="mt-12 flex flex-col gap-4">
+        <form className="mt-12 flex flex-col gap-4">
           <label className="text-sm text-gray-700">Username</label>
           <input
             type="text"
