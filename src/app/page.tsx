0b70bb848a1f255397671ca3a7709b93b20ee199
Elddar00 <eldaridrizovic00@gmail.com>
2024-09-01 01:12:37 +0200

    HomePage - test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,9 @@ import Slider from "@/components/Slider";
 import { WixClientContext } from "@/context/wixContext";
 import { useWixClient } from "@/hooks/useWixClient";
 import { wixClientServer } from "@/lib/wixClientServer";
+import { Suspense, useContext, useEffect } from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
   // const wixClient = useWixClient();
 
   // useEffect(() => {
@@ -44,9 +45,9 @@ const HomePage = () => {
         <h1 className="text-2xl  px-4 md:px-8 xl:px-32 2xl:px-64 mb-12">
           Categories
         </h1>
-        {/* <Suspense fallback={<Skeleton />}> */}
-        <CategoryList />
-        {/* </Suspense> */}
+        {/* <Suspense fallback={<Skeleton />}>
+          <CategoryList />
+        </Suspense> */}
       </div>
       <div className="mt-24 px-4 md:px-8 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
