27b1d041b2dee7e3e5687c3cb8cf31ccc867c063
Elddar00 <eldaridrizovic00@gmail.com>
2024-08-31 02:27:35 +0200

    HomePage - restore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ import Slider from "@/components/Slider";
 import { WixClientContext } from "@/context/wixContext";
 import { useWixClient } from "@/hooks/useWixClient";
 import { wixClientServer } from "@/lib/wixClientServer";
-// import { Suspense, useContext, useEffect } from "react";
+import { Suspense, useContext, useEffect } from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
   // const wixClient = useWixClient();
 
   // useEffect(() => {
@@ -33,20 +33,20 @@ const HomePage = () => {
       <Slider />
       <div className="mt-24 px-4 md:px-8 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        {/* <Suspense fallback={<Skeleton />}>
+        <Suspense fallback={<Skeleton />}>
           <ProductList
             categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
             limit={4}
           />
-        </Suspense> */}
+        </Suspense>
       </div>
       <div className="mt-24 ">
         <h1 className="text-2xl  px-4 md:px-8 xl:px-32 2xl:px-64 mb-12">
           Categories
         </h1>
-        {/* <Suspense fallback={<Skeleton />}>
+        <Suspense fallback={<Skeleton />}>
           <CategoryList />
-        </Suspense> */}
+        </Suspense>
       </div>
       <div className="mt-24 px-4 md:px-8 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
