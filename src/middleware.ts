9ac5a6e4457605d60dafd56b2913087b295683d6
Elddar00 <eldaridrizovic00@gmail.com>
2024-09-02 18:15:20 +0200

    Middelware restore

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,12 +19,12 @@ export async function middleware(request: NextRequest) {
     // Generiši tokene
     const tokens = await wixClient.auth.generateVisitorTokens();
     // Postavi "refreshToken" kolačić
-    // res.cookies.set("refreshToken", JSON.stringify(tokens.refreshToken), {
-    //   maxAge: 60 * 60 * 24 * 30, // 30 dana
-    //   path: "/", // Dostupno za sve rute
-    //   httpOnly: true, // Ne može se pristupiti putem JavaScript-a
-    //   secure: process.env.NODE_ENV === "production", // Samo za HTTPS u produkciji
-    // });
+    res.cookies.set("refreshToken", JSON.stringify(tokens.refreshToken), {
+      maxAge: 60 * 60 * 24 * 30, // 30 dana
+      path: "/", // Dostupno za sve rute
+      httpOnly: true, // Ne može se pristupiti putem JavaScript-a
+      secure: process.env.NODE_ENV === "production", // Samo za HTTPS u produkciji
+    });
   } catch (error) {
     console.error("Error generating visitor tokens:", error);
   }
