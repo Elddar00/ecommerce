237235b03b1e26571ffa85f3de0ccc93b12a42de
Elddar00 <eldaridrizovic00@gmail.com>
2024-09-03 03:48:41 +0200

    HomePage & CategoryList

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,9 +21,8 @@ export async function middleware(request: NextRequest) {
     // Postavi "refreshToken" kolačić
     res.cookies.set("refreshToken", JSON.stringify(tokens.refreshToken), {
       maxAge: 60 * 60 * 24 * 30, // 30 dana
-      path: "/", // Dostupno za sve rute
-      httpOnly: true, // Ne može se pristupiti putem JavaScript-a
-      secure: process.env.NODE_ENV === "production", // Samo za HTTPS u produkciji
+      path: "/",
+      httpOnly: true,
     });
   } catch (error) {
     console.error("Error generating visitor tokens:", error);
