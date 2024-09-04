4197e60c3cd5cbb2bb6c9c5c6a19711668c08772
Elddar00 <eldaridrizovic00@gmail.com>
2024-09-04 15:54:49 +0200

    HomePage - ProducList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,12 @@ import { useWixClient } from "@/hooks/useWixClient";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { Suspense, useContext, useEffect } from "react";
 
-const HomePage = () => {
+const HomePage = async () => {
   // const wixClient = useWixClient();
 
   // useEffect(() => {
-  // const getProducts = async () => {
-  //   const res = await wixClient.products.queryProducts().find();
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
 
   //     console.log(res);
   //   };
@@ -34,12 +34,12 @@ const HomePage = () => {
       <Slider />
       <div className="mt-24 px-4 md:px-8 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <Suspense fallback={<Skeleton />}>
+        {/* <Suspense fallback={<Skeleton />}>
           <ProductList
             categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
             limit={4}
           />
-        </Suspense>
+        </Suspense> */}
       </div>
       <div className="mt-24 ">
         <h1 className="text-2xl  px-4 md:px-8 xl:px-32 2xl:px-64 mb-12">
