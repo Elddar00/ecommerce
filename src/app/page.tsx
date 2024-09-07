f3f536f9c708d0a981dcb498f93addbc72dc74fd
Elddar00 <eldaridrizovic00@gmail.com>
2024-09-07 17:14:15 +0200

    Add New Products - ProductList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,10 +51,10 @@ const HomePage = async () => {
       </div>
       <div className="mt-24 px-4 md:px-8 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">New Products</h1>
-        {/* <ProductList
-          categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+        <ProductList
+          categoryId={process.env.NEW_PRODUCTS_CATEGORY_ID!}
           limit={4}
-        /> */}
+        />
       </div>
     </div>
   );
