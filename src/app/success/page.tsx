8550c3ae8e97e9628fba91d1f035f4cd96d6f63e
Elddar00 <eldaridrizovic00@gmail.com>
2024-09-12 00:32:16 +0200

    Succes Page - Text

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -24,11 +24,9 @@ const SuccessContent = () => {
   return (
     <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
       <Confetti width={2000} height={1000} />
-      <h1 className="text-6xl text-green-700">
-        Hvala Vam na ukazanom poverenju
-      </h1>
-      <h2 className="text-xl font-medium">Vasa porudzbina je evidentirana.</h2>
-      <h3 className="">Podaci Vase porudzbine...</h3>
+      <h1 className="text-6xl text-green-700">Uspesno</h1>
+      <h2 className="text-xl font-medium">Hvala Vam na ukazanom poverenju</h2>
+      <h3 className="">Vasa porudzbina je evidentirana.</h3>
     </div>
   );
 };
