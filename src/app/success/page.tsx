a76b3cdc1c402431114b6e0126aaf71acdd60197
Elddar00 <eldaridrizovic00@gmail.com>
2024-09-11 23:33:28 +0200

    Succes Page - Change Text

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -24,11 +24,11 @@ const SuccessContent = () => {
   return (
     <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
       <Confetti width={2000} height={1000} />
-      <h1 className="text-6xl text-green-700">Successful</h1>
-      <h2 className="text-xl font-medium">
-        We sent the invoice to your e-mail
-      </h2>
-      <h3 className="">You are being redirected to the order page...</h3>
+      <h1 className="text-6xl text-green-700">
+        Hvala Vam na ukazanom poverenju
+      </h1>
+      <h2 className="text-xl font-medium">Vasa porudzbina je evidentirana.</h2>
+      <h3 className="">Podaci Vase porudzbine...</h3>
     </div>
   );
 };
